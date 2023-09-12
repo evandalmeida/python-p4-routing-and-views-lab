b07ffd421fd0596b26fcedc90339603d085c1282
evandalmeida <edalmei1@gmail.com>
2023-09-12 13:39:38 -0400

    completed

--- server/app.py
+++ server/app.py
@@ -4,5 +4,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def index():
+    return ('<h1>Python Operations with Flask Routing and Views</h1>')
+    
+@app.route('/print/<parameter>')
+def print_parameter(parameter):
+    print(parameter) 
+    return parameter
+
+@app.route('/count/<parameter>')
+def count(parameter):
+    param_int = int(parameter)
+    return '\n'.join(str(i) for i in range(param_int + 1))
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
